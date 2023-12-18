554a4907c9382516a5fb46ec71366e878abd4acb
Your Name <you@example.com>
2023-12-18 09:42:00 +0300

    HW3 taskWithAsteriskIsCompleted=false

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,45 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(input string) []string {
+	words := strings.Fields(input)
+
+	wordFreq := make(map[string]int)
+	for _, word := range words {
+		wordFreq[word]++
+	}
+
+	type wordCount struct {
+		word  string
+		count int
+	}
+
+	//nolint:prealloc
+	var wordCounts []wordCount
+	for word, count := range wordFreq {
+		wordCounts = append(wordCounts, wordCount{word, count})
+	}
+
+	sort.Slice(wordCounts, func(i, j int) bool {
+		if wordCounts[i].count == wordCounts[j].count {
+			return wordCounts[i].word < wordCounts[j].word
+		}
+		return wordCounts[i].count > wordCounts[j].count
+	})
+
+	top := 10
+	if len(wordCounts) < top {
+		top = len(wordCounts)
+	}
+
+	result := make([]string, top)
+	for i := 0; i < top; i++ {
+		result[i] = wordCounts[i].word
+	}
+
+	return result
 }
