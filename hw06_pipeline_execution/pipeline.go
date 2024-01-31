b4d25a82f6dfe5507b3eb46e043bf30d51e1cf06
Your Name <you@example.com>
2024-01-31 23:58:11 +0300

    начало работ

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,71 @@
 package hw06pipelineexecution
 
+// Типы каналов для удобства использования в пайплайне
 type (
 	In  = <-chan interface{}
 	Out = In
 	Bi  = chan interface{}
 )
 
+// Stage представляет собой функцию-стейдж для пайплайна
 type Stage func(in In) (out Out)
 
+// ExecutePipeline запускает конкуррентный пайплайн, состоящий из стейджей
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+
+	// Запуск каждого стейджа в пайплайне
+	for _, stage := range stages {
+		out = runStage(out, done, stage)
+	}
+
+	return out
+}
+
+// runStage обрабатывает выполнение отдельного стейджа в горутине
+func runStage(in In, done In, stage Stage) Out {
+	out := make(chan interface{})
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case data, ok := <-in:
+				if !ok {
+					return
+				}
+
+				// Вызов обертки для стейджа с данными из входного канала
+				result := stageWrapper(stage, data)
+				select {
+				case <-done:
+					return
+				case out <- result:
+				}
+			}
+		}
+	}()
+
+	return out
+}
+
+// stageWrapper создает обертку для стейджа, позволяя ему принимать и возвращать данные через каналы
+func stageWrapper(stage Stage, data interface{}) interface{} {
+	out := make(chan interface{})
+	in := make(chan interface{}, 1)
+
+	in <- data
+
+	go func() {
+		defer close(out)
+		close(in)
+		stageOut := stage(in)
+		for data := range stageOut {
+			out <- data
+		}
+	}()
+
+	return <-out
 }
