2e68f8bc35e5a4714f7cdb887e7fd74ef49c4338
Your Name <you@example.com>
2024-02-23 23:02:44 +0300

    hw8 my finish

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,16 +1,73 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
-// EnvValue helps to distinguish between empty files and files with the first empty line.
 type EnvValue struct {
 	Value      string
 	NeedRemove bool
 }
 
-// ReadDir reads a specified directory and returns map of env variables.
-// Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	// получаем список файлов
+	dirEntry, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	envData := make(Environment)
+	// экспортируем всё окружение в карту данных
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		key := pair[0]
+		value := pair[1]
+		envData[key] = EnvValue{Value: value}
+	}
+
+	for _, file := range dirEntry {
+		if file.IsDir() {
+			continue // пропустить директории
+		}
+
+		// проверяем, что это файл
+		fileInfo, err := file.Info()
+		if err != nil {
+			return nil, err
+		}
+
+		// Пропускаем файл если он не регулярный.
+		if !fileInfo.Mode().IsRegular() {
+			continue
+		}
+
+		// Начинаем обработку регулярного файла.
+		fileName := file.Name() // вытаскиваем имя строковое файла
+		// проверяем, что имя файла содержит символ "="
+		if strings.Contains(fileName, "=") {
+			return nil, fmt.Errorf("invalid character '=' in file name: %s", fileName)
+		}
+		// проверяем на размер в 0 байт
+		if fileInfo.Size() == 0 {
+			// проверяем присутствует ли этот env в карте со всем окружением
+			if _, ok := envData[fileName]; ok {
+				// если обнаружен, то выставляем соответсвующий флаг
+				envData[fileName] = EnvValue{NeedRemove: true}
+			}
+			continue // и переходим к следующему
+		}
+		// отправляем на извлечение параметра через разбор первой строчки файла
+		data, err := FileRead(filepath.Join(dir, fileName))
+		if err != nil {
+			return nil, err
+		}
+		envData[fileName] = EnvValue{Value: data}
+	}
+
+	return envData, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,39 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	// Создаем новый командный объект.
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+
+	// Устанавливаем пользовательские переменные среды, если они есть.
+	for key, value := range env {
+		if !value.NeedRemove {
+			command.Env = append(command.Env, key+"="+value.Value)
+		}
+	}
+
+	// Устанавливаем стандартные потоки ввода/вывода/ошибок.
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Stdin = os.Stdin
+
+	// Запускаем команду.
+	err := command.Run()
+	if err != nil {
+		// Если возникла ошибка, проверяем, является ли это ошибкой выполнения команды.
+		if exitError, ok := err.(*exec.ExitError); ok { //nolint:errorlint
+			// Если это ошибка выполнения, возвращаем код возврата.
+			return exitError.ExitCode()
+		}
+		// Если это другая ошибка, то падаем с 111 без объяснения причин
+		os.Exit(111)
+	}
+
+	// Если команда выполнена успешно, возврат 0.
+	return 0
 }
